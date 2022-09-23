3cbcf30036dfa4e6624c35fcef55c8abe21d300a
minhtri228 <63711185+minhtri228@users.noreply.github.com>
2022-09-23 15:54:17 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
+const contactsRouter = require("./app/routes/contact.route");
 app.use(cors());
 app.use(express.json());
+app.use("/api/contacts", contactsRouter);
 app.get("/",(req,res) => {
     res.json({ message:"Welcome to contact book application."});
 });
---
+++ app/routes/contact.route.js
@@ -0,0 +1,14 @@
+const express =require("express");
+const contacts =require("../controllers/contact.controller");
+const router =express.Router();
+router.route("/")
+    .get(contacts.findAll)
+    .post(contacts.create)
+    .delete(contacts.deleteAll);
+router.route("/favorite")
+    .get(contacts.findAllFavourite);
+
+router.route("/:id")
+    .get(contacts.findOne)
+    .put(contacts.update)
+    .delete(contacts.delete)
